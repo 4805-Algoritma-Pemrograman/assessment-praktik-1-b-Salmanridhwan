40eaa1b35a1bc11e6767532eb74f18209e7562f1
Salmanridhwan <salmanridhwan04@gmail.com>
2024-10-24 09:59:46 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,6 +2,16 @@
 
 public class Nomor1{
     public static void main(String[] args){
-      
+        int totalPenjualan;
+        double komisi;
+        
+        if (totalPenjualan >= 500000) {
+            komisi = totalPenjualan * 0.15;
+        }
+        else if (totalPenjualan == 500000) {
+            komisi = totalPenjualan * 0.1;
+        }
+        System.out.println("total penjualan salesman :" + totalPenjualan);
+        System.out.println("komisi yang didapatkan :" + komisi);
     }
 }
